90d1dce6500aae4beeed31472ee621a3f9540a7f
Maria Clara <lu.viniteixeira78@gmail.com>
2024-09-12 19:31:12 -0300

    feat: realizando disparos

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame
 import time
 import sys
+import random
 
 pygame.init()
 
@@ -8,7 +9,7 @@ width, height = 640, 480
 screen = pygame.display.set_mode((width,height))
 back_image = pygame.image.load("Space.png")
 
-
+# Nave
 nave = pygame.image.load("Nave.bmp")
 naverect = nave.get_rect()
 naverect.x = 320
@@ -17,12 +18,45 @@ naverect.y = 420
 # Tiro
 tiro = pygame.image.load("Nave.bmp")
 tiroRect = tiro.get_rect()
-tiroRect.x = 0
-tiroRect.y = 420
+
+# Monstro
+monstro = pygame.image.load("Nave.bmp")
+monstroRect = monstro.get_rect()
+listaMonstro = []
+listaMonstro.append(monstroRect)
+listaMonstro.append(monstroRect)
+listaMonstro.append(monstroRect)
+listaMonstro.append(monstroRect)
+listaMonstro.append(monstroRect)
 
 
-def tiro(x,y):
+def atirar(localNave):
+    tiroRect.x = localNave
+    tiroRect.y -= 4
     screen.blit(tiro, tiroRect)
+
+# qtdMonstro = 0
+# while qtdMonstro < 5:
+#     listaMonstro[qtdMonstro].x = random.randint(100, 600)
+#     listaMonstro[qtdMonstro].y = random.randint(30, 200)
+
+#     qtdMonstro += 1
+
+qtdM = 0
+def blit():
+    screen.blit(tiro, tiroRect)
+    screen.blit(back_image, (0,0))
+    screen.blit(nave, naverect)
+
+
+    while qtdM < 5:
+        listaMonstro[qtdM].x = random.randint(200, 300)
+
+        listaMonstro[qtdM].y = random.randint(200, 300)
+
+        screen.blit(monstro, listaMonstro[qtdM])
+
+        qtdM += 1
 
 while True:
     for event in pygame.event.get():
@@ -30,7 +64,6 @@ while True:
             pygame.quit()
             sys.quit()
 
-    
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_a]:
@@ -44,11 +77,30 @@ while True:
             naverect.right = 640
 
     if keys[pygame.K_SPACE]:
-        tiro(naverect.x, tiroRect.y)
+        tiroRect.x = naverect.x
+        tiroRect.y = 420
+        while tiroRect.y > 30:
+            atirar(naverect.x)
+            pygame.display.flip()
+            blit()
+
+    blit()
+
+    # listaMonstro[0].x = 300
+
+    # listaMonstro[0].y = 100
+
+    # screen.blit(monstro, listaMonstro[0])
+
+    # listaMonstro[1].x = 200
+    # listaMonstro[1].y = 300
+    # screen.blit(monstro,listaMonstro[1])
+
+    # listaMonstro[2].x = 100
+    # listaMonstro[2].y = 200
+    # screen.blit(monstro,listaMonstro[2])
 
 
-    screen.blit(back_image, (0,0))
-    screen.blit(nave, naverect)
     pygame.display.flip()
 
     time.sleep(0.015)
